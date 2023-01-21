e015cf7cfedec40b23ffb72b78aedbea94a9dd93
Brandon Phillips <brandon.phillips@bphillips.dev>
2023-01-20 22:38:37 -0800

    Make site colors more pleasant

--- packages/server/src/controllers/university.controllers.ts
+++ packages/server/src/controllers/university.controllers.ts
@@ -2,14 +2,13 @@ import { UniversityApiEndpoints } from "@university-hub/shared/api/endpoints";
 import { ApiHandler } from ".";
 
 export const GetUniversityShallowController: ApiHandler<UniversityApiEndpoints.GetUniversityShallow> = (req, res) => {
-    console.log("hi");
     res.json({
         siteColors: {
-            primaryBgColor: "rgb(100, 0, 0)",
-            secondaryBgColor: "rgb(0, 0, 0)",
-            primaryTextColor: "rgb(0, 0, 0)",
+            primaryBgColor: "rgb(23, 24, 30)",
+            secondaryBgColor: "rgb(255, 255, 255)",
+            primaryTextColor: "rgb(255, 255, 255)",
             secondaryTextColor: "rgb(0, 0, 0)",
-          },
+            },
         logoUrl: "some.url"
     }).end();
 }
